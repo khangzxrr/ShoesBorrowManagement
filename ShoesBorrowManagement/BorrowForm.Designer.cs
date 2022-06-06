8a57605c03d9264006f7f75ebb00b2214ad64a8e
khangzxrr <vokhinbabezx4@gmail.com>
2022-06-06 21:45:03 +0700

    Fix 1080p Monitor display

--- ShoesBorrowManagement/BorrowForm.Designer.cs
+++ ShoesBorrowManagement/BorrowForm.Designer.cs
@@ -54,33 +54,35 @@
             // 
             this.unborrowedDatagrid.AllowUserToAddRows = false;
             this.unborrowedDatagrid.AllowUserToDeleteRows = false;
-            this.unborrowedDatagrid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.unborrowedDatagrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.unborrowedDatagrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.unborrowedDatagrid.Location = new System.Drawing.Point(1601, 243);
+            this.unborrowedDatagrid.Location = new System.Drawing.Point(862, 114);
+            this.unborrowedDatagrid.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.unborrowedDatagrid.Name = "unborrowedDatagrid";
             this.unborrowedDatagrid.ReadOnly = true;
             this.unborrowedDatagrid.RowHeadersWidth = 82;
             this.unborrowedDatagrid.RowTemplate.Height = 41;
             this.unborrowedDatagrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.unborrowedDatagrid.Size = new System.Drawing.Size(803, 775);
+            this.unborrowedDatagrid.Size = new System.Drawing.Size(430, 363);
             this.unborrowedDatagrid.TabIndex = 0;
             // 
             // borrowGrid
             // 
             this.borrowGrid.AllowUserToAddRows = false;
             this.borrowGrid.AllowUserToDeleteRows = false;
-            this.borrowGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.borrowGrid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.borrowGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.borrowGrid.Location = new System.Drawing.Point(41, 252);
+            this.borrowGrid.Location = new System.Drawing.Point(22, 118);
+            this.borrowGrid.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.borrowGrid.Name = "borrowGrid";
             this.borrowGrid.ReadOnly = true;
             this.borrowGrid.RowHeadersWidth = 82;
             this.borrowGrid.RowTemplate.Height = 41;
             this.borrowGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.borrowGrid.Size = new System.Drawing.Size(811, 766);
+            this.borrowGrid.Size = new System.Drawing.Size(445, 359);
             this.borrowGrid.TabIndex = 1;
             this.borrowGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.borrowGrid_onCellClick);
             // 
@@ -88,30 +90,31 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(41, 77);
+            this.label1.Location = new System.Drawing.Point(22, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(321, 50);
+            this.label1.Size = new System.Drawing.Size(165, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Giày đã cho mượn";
             // 
             // label2
             // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(1610, 72);
+            this.label2.Location = new System.Drawing.Point(865, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(359, 50);
+            this.label2.Size = new System.Drawing.Size(185, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "Giày chưa cho mượn";
             // 
             // borrowShoeBtn
             // 
-            this.borrowShoeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.borrowShoeBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.borrowShoeBtn.Location = new System.Drawing.Point(1601, 147);
+            this.borrowShoeBtn.Location = new System.Drawing.Point(860, 63);
+            this.borrowShoeBtn.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.borrowShoeBtn.Name = "borrowShoeBtn";
-            this.borrowShoeBtn.Size = new System.Drawing.Size(243, 64);
+            this.borrowShoeBtn.Size = new System.Drawing.Size(131, 30);
             this.borrowShoeBtn.TabIndex = 5;
             this.borrowShoeBtn.Text = "Cho mượn";
             this.borrowShoeBtn.UseVisualStyleBackColor = true;
@@ -120,9 +123,10 @@
             // deleteBorrowButton
             // 
             this.deleteBorrowButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.deleteBorrowButton.Location = new System.Drawing.Point(41, 165);
+            this.deleteBorrowButton.Location = new System.Drawing.Point(22, 77);
+            this.deleteBorrowButton.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.deleteBorrowButton.Name = "deleteBorrowButton";
-            this.deleteBorrowButton.Size = new System.Drawing.Size(243, 64);
+            this.deleteBorrowButton.Size = new System.Drawing.Size(131, 30);
             this.deleteBorrowButton.TabIndex = 6;
             this.deleteBorrowButton.Text = "Xóa";
             this.deleteBorrowButton.UseVisualStyleBackColor = true;
@@ -131,16 +135,18 @@
             // groupBox1
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.groupBox1.Controls.Add(this.showBorrowDateCount);
             this.groupBox1.Controls.Add(this.shoeBorrowDate);
             this.groupBox1.Controls.Add(this.shoeDetail);
             this.groupBox1.Controls.Add(this.shoeSize);
             this.groupBox1.Controls.Add(this.shoeCatalog);
             this.groupBox1.Controls.Add(this.shoeName);
-            this.groupBox1.Location = new System.Drawing.Point(881, 252);
+            this.groupBox1.Location = new System.Drawing.Point(499, 118);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(628, 639);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.groupBox1.Size = new System.Drawing.Size(322, 300);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thông tin giày";
@@ -149,9 +155,10 @@
             // 
             this.showBorrowDateCount.AutoSize = true;
             this.showBorrowDateCount.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.showBorrowDateCount.Location = new System.Drawing.Point(28, 518);
+            this.showBorrowDateCount.Location = new System.Drawing.Point(15, 243);
+            this.showBorrowDateCount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.showBorrowDateCount.Name = "showBorrowDateCount";
-            this.showBorrowDateCount.Size = new System.Drawing.Size(182, 50);
+            this.showBorrowDateCount.Size = new System.Drawing.Size(93, 25);
             this.showBorrowDateCount.TabIndex = 5;
             this.showBorrowDateCount.Text = "Đã mượn:";
             // 
@@ -159,9 +166,10 @@
             // 
             this.shoeBorrowDate.AutoSize = true;
             this.shoeBorrowDate.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.shoeBorrowDate.Location = new System.Drawing.Point(28, 412);
+            this.shoeBorrowDate.Location = new System.Drawing.Point(15, 193);
+            this.shoeBorrowDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.shoeBorrowDate.Name = "shoeBorrowDate";
-            this.shoeBorrowDate.Size = new System.Drawing.Size(224, 50);
+            this.shoeBorrowDate.Size = new System.Drawing.Size(114, 25);
             this.shoeBorrowDate.TabIndex = 4;
             this.shoeBorrowDate.Text = "Ngày mượn:";
             // 
@@ -169,9 +177,10 @@
             // 
             this.shoeDetail.AutoSize = true;
             this.shoeDetail.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.shoeDetail.Location = new System.Drawing.Point(28, 313);
+            this.shoeDetail.Location = new System.Drawing.Point(15, 147);
+            this.shoeDetail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.shoeDetail.Name = "shoeDetail";
-            this.shoeDetail.Size = new System.Drawing.Size(147, 50);
+            this.shoeDetail.Size = new System.Drawing.Size(76, 25);
             this.shoeDetail.TabIndex = 3;
             this.shoeDetail.Text = "Chi tiết:";
             // 
@@ -179,9 +188,10 @@
             // 
             this.shoeSize.AutoSize = true;
             this.shoeSize.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.shoeSize.Location = new System.Drawing.Point(28, 227);
+            this.shoeSize.Location = new System.Drawing.Point(15, 106);
+            this.shoeSize.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.shoeSize.Name = "shoeSize";
-            this.shoeSize.Size = new System.Drawing.Size(95, 50);
+            this.shoeSize.Size = new System.Drawing.Size(50, 25);
             this.shoeSize.TabIndex = 2;
             this.shoeSize.Text = "Size:";
             // 
@@ -189,9 +199,10 @@
             // 
             this.shoeCatalog.AutoSize = true;
             this.shoeCatalog.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.shoeCatalog.Location = new System.Drawing.Point(28, 139);
+            this.shoeCatalog.Location = new System.Drawing.Point(15, 65);
+            this.shoeCatalog.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.shoeCatalog.Name = "shoeCatalog";
-            this.shoeCatalog.Size = new System.Drawing.Size(97, 50);
+            this.shoeCatalog.Size = new System.Drawing.Size(51, 25);
             this.shoeCatalog.TabIndex = 1;
             this.shoeCatalog.Text = "Loại:";
             // 
@@ -199,9 +210,10 @@
             // 
             this.shoeName.AutoSize = true;
             this.shoeName.Font = new System.Drawing.Font("Segoe UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.shoeName.Location = new System.Drawing.Point(28, 60);
+            this.shoeName.Location = new System.Drawing.Point(15, 28);
+            this.shoeName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.shoeName.Name = "shoeName";
-            this.shoeName.Size = new System.Drawing.Size(85, 50);
+            this.shoeName.Size = new System.Drawing.Size(45, 25);
             this.shoeName.TabIndex = 0;
             this.shoeName.Text = "Tên:";
             // 
@@ -213,29 +225,30 @@
             this.quảnLíLoạiGiàyToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(2477, 40);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(3, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(1306, 24);
             this.menuStrip1.TabIndex = 9;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // quảnLíGiàyToolStripMenuItem
             // 
             this.quảnLíGiàyToolStripMenuItem.Name = "quảnLíGiàyToolStripMenuItem";
-            this.quảnLíGiàyToolStripMenuItem.Size = new System.Drawing.Size(162, 36);
+            this.quảnLíGiàyToolStripMenuItem.Size = new System.Drawing.Size(82, 22);
             this.quảnLíGiàyToolStripMenuItem.Text = "Quản lí giày";
             this.quảnLíGiàyToolStripMenuItem.Click += new System.EventHandler(this.managed_shoe);
             // 
             // quảnLíLoạiGiàyToolStripMenuItem
             // 
             this.quảnLíLoạiGiàyToolStripMenuItem.Name = "quảnLíLoạiGiàyToolStripMenuItem";
-            this.quảnLíLoạiGiàyToolStripMenuItem.Size = new System.Drawing.Size(207, 36);
+            this.quảnLíLoạiGiàyToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
             this.quảnLíLoạiGiàyToolStripMenuItem.Text = "Quản lí loại giày";
             this.quảnLíLoạiGiàyToolStripMenuItem.Click += new System.EventHandler(this.managed_catalog);
             // 
             // BorrowForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2477, 1036);
+            this.ClientSize = new System.Drawing.Size(1306, 486);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.deleteBorrowButton);
             this.Controls.Add(this.borrowShoeBtn);
@@ -245,7 +258,6 @@
             this.Controls.Add(this.unborrowedDatagrid);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "BorrowForm";
             this.Text = "BorrowForm";
             ((System.ComponentModel.ISupportInitialize)(this.unborrowedDatagrid)).EndInit();
